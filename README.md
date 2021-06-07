ecf1a138a33e69b3c724d528d4194d00dfd8bf6b
Matt Dudzik <mdudzik92@gmail.com>
2021-06-07 17:38:33 -0500

    Added user ability to choose their own speed read time, updated README.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # JS-Speed-Reader
+
+JavaScript Speed Reader
+
+<!-- Live link to deployed app -->
+
+Repository: https://github.com/Mdudzik92/JS-Speed-Reader<br>
+Deployed app: https://mdudzik92.github.io/js-speed-reader/
+
+<!-- Technologies used -->
+
+JavaScript, HTML, DOM Manipulation
+
+<!-- Explanation of what the app is -->
+
+This is a speed reader app created with JavaScript using DOM Manipulation to access and write to a blank HTML file. The user opens the page and chooses how many seconds between words they would like to test their speed reading skills with, letting them know they need to choose in milliseconds. They watch as the time counter decrements by one second at a time until each word of a specific poem is then printed to the screen at the time interval they picked.
+
+<!-- Screenshot -->
+
+<img src="./img/img1.png">
+<img src="./img/img2.png">
+<img src="./img/img3.png">
+<img src="./img/img4.png">
+
+<!-- License -->
+
+MIT
+
+<!-- Contact information -->
+
+Email: mdudzik92@gmail.com
--- script.js
+++ script.js
@@ -8,8 +8,17 @@ var words = poem.split(" ");
 // Grabbing the HTML tags and setting them in their own variables.
 var mainEl = document.getElementById("main");
 var timerEl = document.getElementById("countdown");
+
 // Creating a div to set the poem into later.
 var bodyEl = document.createElement("div");
+
+// Setting what i is to be read as in the second function. Effectively turning the poem into an array.
+var i = 0;
+
+// Allows the user to choose how many milliseconds between each word they should get printed to the screen, thus letting them work at their preferred pace for speed reading.
+var millisecondsPerWord = prompt(
+	"How many milliseconds between words would you like? 1000 milliseconds = 1 second.",
+);
 
 // Creating the prepareRead function to set our countdown timer process in.
 function prepareRead() {
@@ -18,5 +27,33 @@ function prepareRead() {
 	var timerInterval = setInterval(function () {
 		timerEl.textContent = secondsLeft + " seconds remaining";
 		secondsLeft--;
-	});
+
+		// Setting the if statement so when the timer runs out it clears the timer and begins the speedRead function.
+		if (secondsLeft === 0) {
+			timerEl.textContent = "";
+			// Begins second function, clearing timer from the screen.
+			speedRead();
+			clearInterval(timerInterval);
+		}
+	}, 1000);
 }
+
+// Creating the speedRead function to start the speed reading process.
+function speedRead() {
+	// First I'm appending a div to the h3 in the HTML.
+	mainEl.append(bodyEl);
+
+	// Setting the function to print the words to the screen one at a time.
+	var poemInterval = setInterval(function () {
+		if (words[i] === undefined) {
+			clearInterval(poemInterval);
+		} else {
+			// Start the words one at a time. The 'words' variable uses the split method which reads the poem as an array. Here I have it reading each word and going through the array one by one, word for word.
+			mainEl.textContent = words[i];
+			i++;
+		}
+	}, millisecondsPerWord);
+}
+
+// Starting the process.
+prepareRead();
